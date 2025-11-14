f62ec467110cc7a6f44e40293ebbeeb0f5367a50
Angelo Pirola <angelo9871@gmail.com>
2025-11-14 11:49:11 +0100

    Add UnitTestDbContext with seeded test data
    Introduced a new `UnitTestDbContext` class to facilitate testing, including `DbSet` properties for `Person` and `Address` entities.
    
    Used the `Bogus` library to generate and seed 10 fake `Address` entities and 10 fake `Person` entities with Italian locale. Configured relationships between `Person` and `Address` entities using `HasData` in the `OnModelCreating` method.

---
+++ src/ClassLibrary.EFCore.Tests/DatabaseContext/UnitTestDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using ClassLibrary.EFCore.Tests.Entities;
+using Microsoft.EntityFrameworkCore;
+using Persone = ClassLibrary.EFCore.Tests.Entities.Person;
+
+namespace ClassLibrary.EFCore.Tests.DatabaseContext;
+
+public class UnitTestDbContext(DbContextOptions<UnitTestDbContext> options) : DbContext(options)
+{
+    public DbSet<Persone> Persone { get; set; }
+    public DbSet<Address> Indirizzi { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var fakerAddresses = new Faker<Address>("it")
+            .RuleFor(x => x.Id, f => f.IndexFaker + 1)
+            .RuleFor(x => x.Citta, f => f.Address.City())
+            .RuleFor(x => x.Cap, f => f.Address.ZipCode())
+            .RuleFor(x => x.Provincia, f => f.Address.State());
+
+        var indirizzi = fakerAddresses.Generate(10);
+        modelBuilder.Entity<Address>().HasData(indirizzi);
+
+        var fakerPeople = new Faker<Persone>("it")
+            .RuleFor(x => x.Id, f => f.IndexFaker + 1)
+            .RuleFor(x => x.Cognome, f => f.Person.LastName)
+            .RuleFor(x => x.Nome, f => f.Person.FirstName)
+            .RuleFor(d => d.IndirizzoId, f => f.PickRandom(indirizzi).Id);
+
+        var persone = fakerPeople.Generate(10);
+        modelBuilder.Entity<Persone>().HasData(persone);
+    }
+}
