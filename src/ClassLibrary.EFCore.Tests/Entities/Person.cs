4471a27301fcb7841130fbcc5a4a79e1d7fa1378
Angelo Pirola <angelo9871@gmail.com>
2025-11-14 11:48:52 +0100

    Add `Person` class implementing `IEntity<int>`
    A new `Person` class was added to the `ClassLibrary.EFCore.Tests.Entities` namespace. This class implements the `IEntity<int>` interface and includes the following properties:
    - `Id`: Unique identifier for the `Person`.
    - `Cognome`: Non-nullable string for the last name.
    - `Nome`: Non-nullable string for the first name.
    - `IndirizzoId`: Foreign key for the associated `Address`.
    - `Indirizzo`: Navigation property for the associated `Address`.
    
    Additionally, a `using` directive for `ClassLibrary.EFCore.Interfaces` was added to support the `IEntity<int>` interface.

---
+++ src/ClassLibrary.EFCore.Tests/Entities/Person.cs
@@ -0,0 +1,12 @@
+﻿using ClassLibrary.EFCore.Interfaces;
+
+namespace ClassLibrary.EFCore.Tests.Entities;
+
+public class Person : IEntity<int>
+{
+    public int Id { get; set; }
+    public string Cognome { get; set; } = null!;
+    public string Nome { get; set; } = null!;
+    public int IndirizzoId { get; set; }
+    public Address Indirizzo { get; set; } = default!;
+}
